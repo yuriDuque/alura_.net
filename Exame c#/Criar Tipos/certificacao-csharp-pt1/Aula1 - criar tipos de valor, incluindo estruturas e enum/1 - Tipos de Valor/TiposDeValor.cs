5141aa96e6377d08733eb54fabab73ea9e0626bd
YuriDuque <yurithielmann83@gmail.com>
2020-02-18 22:32:01 -0300

    primeiro commit

---
+++ Exame c#/Criar Tipos/certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/1 - Tipos de Valor/TiposDeValor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace certificacao_csharp_roteiro
+{
+    class TiposDeValor : IAulaItem
+    {
+        public void Executar()
+        {
+            int idade;
+            idade = 30;
+            Console.WriteLine(idade);
+
+            int copiaIdade = idade;
+            Console.WriteLine($"Idade: {idade}");
+            Console.WriteLine($"Copia idade: {copiaIdade}");
+
+            idade = 23;
+            Console.WriteLine($"Idade: {idade}");
+            Console.WriteLine($"Copia idade: {copiaIdade}");
+
+            int? idadeNula = null;
+        }
+    }
+}
---
+++ Exame c#/Criar Tipos/certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/2 - Tipos Inteiros/Inteiros.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class TiposInteiros : IAulaItem
+    {
+        public void Executar()
+        {
+            short passageirosVoo = -230; // System.Int16 - aceita negativo
+            ushort passageirosNavio = 230; // System.UInt16 - não aceita negativo
+
+            int idade = -15; // System.Int32 - aceita negativo
+            uint estoque = 15; // System.UInt32 - não aceita negativo
+
+            long populacaoDoBrasil = -207_660_920; // System.Int64 - aceita negativo
+            ulong populacaoDoMundo = 7_000_000_000; // System.UInt64 - aceita negativo
+
+            char resposta = 'S'; // System.Char
+
+            byte nivelDeAzul = 0xFF; // = 255 em decimal - não aceita negativo
+            sbyte nivelDeBrilho = -127; // System.SByte - aceita negativo
+
+            Console.WriteLine($"short: {passageirosVoo}");
+            Console.WriteLine($"ushort: {passageirosNavio}");
+
+            Console.WriteLine($"int: {idade}");
+            Console.WriteLine($"uint: {estoque}");
+
+            Console.WriteLine($"long: {populacaoDoBrasil}");
+            Console.WriteLine($"ulong: {populacaoDoMundo}");
+
+            Console.WriteLine($"char: {resposta}");
+
+            Console.WriteLine($"byte: {nivelDeAzul}");
+            Console.WriteLine($"sbyte: {nivelDeBrilho}");
+
+            // TIPOS INTEGRAIS
+
+            // TIPO          BYTE          ACEITA NEGATIVO?
+            // char          1             N
+            // byte          1             N
+            // sbyte         1             S
+            // short         2             S
+            // ushort        2             N
+            // int           4             S
+            // uint          4             N
+            // long          8             S
+            // ulong         8             N
+
+
+
+            // TIPOS DE VALOR 
+
+            // TIPO          CATEGORIA
+            // char          número inteiro
+            // byte          número inteiro
+            // sbyte         número inteiro
+            // short         número inteiro
+            // ushort        número inteiro
+            // int           número inteiro
+            // uint          número inteiro
+            // long          número inteiro
+            // ulong         número inteiro
+            // bool          booleano
+            // decimal       número decimal
+            // double        número ponto flutuante
+            // float         número ponto flutuante
+            // enum          enumeracao
+            // struct        estrutura
+        }
+    }
+}
